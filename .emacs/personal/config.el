e21b4ab915d75be9de8e894c9f045f047c37c496
Antonio Miranda <antonio.miranda@wuaki.tv>
2015-06-21 11:18:08 +0200

    Move some emacs config from custom to config

--- .emacs/personal/config.el
+++ .emacs/personal/config.el
@@ -8,6 +8,17 @@
 ;; projectile
 (setq projectile-completion-system 'grizzl)
 
+;; helm
+(require 'prelude-helm-everywhere)
+(setq helm-M-x-fuzzy-match t)
+(setq helm-buffers-fuzzy-matching t)
+(setq helm-mode t)
+(setq helm-recentf-fuzzy-match t)
+
+;; key-chord
+(require 'prelude-key-chord)
+(key-chord-mode 1)
+
 ;; indent-guide
 (require 'indent-guide)
 
@@ -56,16 +67,15 @@
 (setq linum-format "%3d ")
 (setq show-trailing-whitespace t)
 
-;; coffee
-(custom-set-variables '(coffee-tab-width 2))
-
+;; tab-width 2
+(setq coffee-tab-width 2)
 (setq js-indent-level 2)
-
-;; json-mode set indent level to 2
 (add-hook 'json-mode-hook
           (lambda ()
             (make-variable-buffer-local 'js-indent-level)
             (setq js-indent-level 2)))
+(setq js2-basic-offset 2)
+(setq json-reformat:indent-width 2)
 
 ;; multiple-cursors
 (require 'multiple-cursors)
@@ -74,3 +84,15 @@
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
+;; neotree
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
+(setq neo-show-hidden-files t)
+(setq neo-theme (quote nerd))
+(setq neo-vc-integration (quote (face char)))
+(setq neo-window-width 30)
+
+;; expand region
+(require 'expand-region)
+(global-set-key (kbd "C-=") 'er/expand-region)
